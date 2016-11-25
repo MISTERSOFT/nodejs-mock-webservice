d4ded5ceb3b281fa1d813e307a995bf867ace94a
= <=>
2016-11-25 14:38:52 +0100

    construct route with express

--- server.js
+++ server.js
@@ -5,13 +5,17 @@
 // Import
 const Constants = require('./constants.js');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 const http = require('http');
 const Database = require('./database.js');
 const REST = require('./Rest.js');
 
 // Variables
+let app = express();
 let httpServer = null;
 let db = new Database();
+let Rest = new REST(db);
 
 // Use this if you want fill the database - Disabled after use
 // db.fillDatabaseWithMockData(10);
@@ -20,76 +24,111 @@ let db = new Database();
 // console.log(db.deleteMockData());
 
 // Init http server
-httpServer = http.createServer( (request, response) => {
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    if (request.url === '/') {
-        _responseServer(response, JSON.stringify({message: 'Nothing here'}));
-    }
-
-    let urlParsed = _parseUrl(request.url);
-
-    if (urlParsed.length > 0) {
-        let Rest = new REST(db);
-
-        // Get
-        if (request.method === 'GET') {
-
-            if (urlParsed.length === 1) {
-                _responseServer(response, Rest.get().getJson());
-            }
-            else {
-                _responseServer(response, Rest.get(urlParsed[1]).getJson());
-            }
-
-        }
-
-        // Post
-        if (request.method === 'POST') {
-
-        }
-        
-        // Put
-        if (request.method === 'PUT') {
-
-        }
-
-        // Delete
-        if (request.method === 'DELETE') {
-
-        }
-
-    }
-    else {
-        _404(response);
-    }
-
-
-    // switch (request.url) {
-    //     case '/':
-    //         response.statusCode = 200;
-    //         response.setHeader('Content-Type', Constants.jsonHeader);
-    //         response.end('<pre>Hello</pre>');
-    //         break;
-
-    //     case '/products':
-    //         response.statusCode = 200;
-    //         response.setHeader('Content-Type', Constants.jsonHeader);
-    //         response.end('Hello');
-    //         break;
-        
-    //     case '/blog':
-    //         response.statusCode = 200;
-    //         response.setHeader('Content-Type', Constants.jsonHeader);
-    //         response.end('Blog');
-    //         break;
-
-    //     default:
-    //         _404();
-    //         break;
-    // }
+app.get('/', (req, res, next) => {
+    let text = '';
+    text += 'Hello !\n';
+    text += 'You can use these routes bellow\n';
+    text += '* GET :\n';
+    text += '/products\n';
+    text += '/products/:id\n';
+    text += '* POST :\n';
+    text += '/products => With params { title: "A title", description: "A description", price: 10 }\n';
+    text += 'TODO'
+    res.end(text);
 });
 
-httpServer.listen(Constants.httpPort, Constants.host, () => {
+app.get('/products', (req, res, next) => {
+    _responseServer(res, Rest.get().getJson());
+});
+
+app.get('/products/:id', (req, res, next) => {
+    res.end('Route: ' + req.url + ' => params = ' + req.params.id);
+});
+
+app.post('/products', (req, res, next) => {
+    // req.body.title
+    res.end('/products => params = ');
+});
+app.get('/products', (req, res, next) => {
+    res.end(JSON.stringify({message: 'bjr'}));
+});
+
+// httpServer = http.createServer( (request, response) => {
+
+//     if (request.url === '/') {
+//         _responseServer(response, JSON.stringify({message: 'Nothing here'}));
+//     }
+
+//     let urlParsed = _parseUrl(request.url);
+
+//     if (urlParsed.length > 0) {
+//         let Rest = new REST(db);
+
+//         // Get
+//         if (request.method === 'GET') {
+
+//             if (urlParsed.length === 1) {
+//                 _responseServer(response, Rest.get().getJson());
+//             }
+//             else {
+//                 _responseServer(response, Rest.get(urlParsed[1]).getJson());
+//             }
+
+//         }
+
+//         // Post
+//         if (request.method === 'POST') {
+
+//         }
+        
+//         // Put
+//         if (request.method === 'PUT') {
+
+//         }
+
+//         // Delete
+//         if (request.method === 'DELETE') {
+
+//         }
+
+//     }
+//     else {
+//         _404(response);
+//     }
+
+
+//     // switch (request.url) {
+//     //     case '/':
+//     //         response.statusCode = 200;
+//     //         response.setHeader('Content-Type', Constants.jsonHeader);
+//     //         response.end('<pre>Hello</pre>');
+//     //         break;
+
+//     //     case '/products':
+//     //         response.statusCode = 200;
+//     //         response.setHeader('Content-Type', Constants.jsonHeader);
+//     //         response.end('Hello');
+//     //         break;
+        
+//     //     case '/blog':
+//     //         response.statusCode = 200;
+//     //         response.setHeader('Content-Type', Constants.jsonHeader);
+//     //         response.end('Blog');
+//     //         break;
+
+//     //     default:
+//     //         _404();
+//     //         break;
+//     // }
+// });
+
+// httpServer.listen(Constants.httpPort, Constants.host, () => {
+//     console.log(`Server running at http://${Constants.host}:${Constants.httpPort}/`);
+// });
+
+app.listen(Constants.httpPort, () => {
     console.log(`Server running at http://${Constants.host}:${Constants.httpPort}/`);
 });
 
